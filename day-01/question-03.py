3c46b099dc712f68f999fe8294b1a1dc8dce4e48
Izna khan <khanizna192@gmail.com>
2026-09-12 23:37:48 +0530

    Reset Day 1 questions to question-only format

--- day-01/question-03.py
+++ day-01/question-03.py
@@ -1,30 +1,6 @@
-def find_prime(number: int) -> str:
-    """
-    Checks whether a number is prime or not.
-
-    Args:
-        number (int): A number to check.
-
-    Returns:
-        str: A string that tells whether a number is prime or not.
-
-    Raises:
-        TypeError: If number is not an integer.
-        ValueError: If number is less than 2.
-    """
-    if not isinstance(number, int):
-        raise TypeError("number must be an integer.")
-    if number < 2:
-        raise ValueError("number must be greater than or equal to 2.")
-
-    for i in range(2, number):
-        if number % i == 0:
-            return f"{number} is not a prime number."
-
-    return f"{number} is prime number."
-
-
-if __name__ == "__main__":
-    number = int(input("Enter a number : "))
-    result = find_prime(number)
-    print(result)
+# 3. Take the input from user and find whether number is prime or not?
+#
+# Prime numbers are number which are divide by 1 or themselves only.
+#
+# Example: 13 is a prime number.
+# Example: 97 is a prime number.

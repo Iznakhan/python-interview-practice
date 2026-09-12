7c409ace6b4e6d2fd22aed3dbb5261cedaef5e0b
Izna khan <khanizna192@gmail.com>
2026-09-12 23:23:01 +0530

    Update question 4

--- day-01/question-04.py
+++ day-01/question-04.py
@@ -1,18 +1,5 @@
-def prime_numbers(start: int, end: int) -> None:
-    """
-    Prints all prime numbers between a given range.
+### 4. Find all prime numbers between 100 and 200.
 
-    Args:
-        start (int): The starting number.
-        end (int): The ending number.
-    """
-    for number in range(start, end + 1):
-        for i in range(2, number):
-            if number % i == 0:
-                break
-        else:
-            print(number)
+### All prime numbers between 100 and 200 are:
 
-
-if __name__ == "__main__":
-    prime_numbers(100, 200)
+### 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199
